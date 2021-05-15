f0cad333d66338ba607a3838b9048e4b0dbe6ba8
skychx <skychx@hotmail.com>
2021-05-15 22:14:13 +0800

    :sparkles:  4.10——「高级OpenGL」——「实例化_实例化渲染 5w 小行星」练习

--- 4.advanced_opengl/main.cpp
+++ 4.advanced_opengl/main.cpp
@@ -75,24 +75,30 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    Shader shader(
+    Shader planetShader(
+            "../shaders/4.10.planet.vert",
+            "../shaders/4.10.planet.frag"
+            );
+    Shader asteroidShader(
             "../shaders/4.10.asteroids.vert",
             "../shaders/4.10.asteroids.frag"
-            );
+    );
 
+    // load models
+    // -----------
     Model rock("../resources/rock/rock.obj");
     Model planet("../resources/planet/planet.obj");
 
     // load models
     // -----------
 
-    // 绘制 1w 个小行星
-    unsigned int amount = 10000;
+    // 绘制 5w 个小行星
+    unsigned int amount = 50000;
     glm::mat4* modelMatrices;
     modelMatrices = new glm::mat4[amount];
     srand(glfwGetTime()); // 初始化随机种子
-    float radius = 50.0;
-    float offset = 2.5f;
+    float radius = 150.0;
+    float offset = 25.0f;
     for (unsigned int i = 0; i < amount; i++) {
         glm::mat4 model = glm::mat4(1.0f);
         // 1. 位移：分布在半径为 'radius' 的圆形上，偏移的范围是 [-offset, offset]
@@ -117,6 +123,41 @@ int main()
         modelMatrices[i] = model;
     }
 
+    // configure instanced array
+    // 顶点缓冲对象
+    // -------------------------
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+
+    // set transformation matrices as an instance vertex attribute (with divisor 1)
+    // note: we're cheating a little by taking the, now publicly declared, VAO of the model's mesh(es) and adding new vertexAttribPointers
+    // normally you'd want to do this in a more organized fashion, but for learning purposes this will do.
+    // -----------------------------------------------------------------------------------------------------------------------------------
+    for (unsigned int i = 0; i < rock.meshes.size(); i++) {
+        unsigned int VAO = rock.meshes[i].VAO;
+        glBindVertexArray(VAO);
+        // set attribute pointers for matrix (4 times vec4)
+        // 因为一个mat4本质上是4个vec4，我们需要为这个矩阵预留4个顶点属性。
+        // 因为我们将它的位置值设置为3，矩阵每一列的顶点属性位置值就是3、4、5和6
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+        glEnableVertexAttribArray(5);
+        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+        glEnableVertexAttribArray(6);
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
+
+        glVertexAttribDivisor(3, 1);
+        glVertexAttribDivisor(4, 1);
+        glVertexAttribDivisor(5, 1);
+        glVertexAttribDivisor(6, 1);
+
+        glBindVertexArray(0);
+    }
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -138,22 +179,30 @@ int main()
 
         // configure transformation matrices
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
-        glm::mat4 view = camera.GetViewMatrix();;
-        shader.use();
-        shader.setMat4("projection", projection);
-        shader.setMat4("view", view);
+        glm::mat4 view = camera.GetViewMatrix();
+        asteroidShader.use();
+        asteroidShader.setMat4("projection", projection);
+        asteroidShader.setMat4("view", view);
+        planetShader.use();
+        planetShader.setMat4("projection", projection);
+        planetShader.setMat4("view", view);
 
         // 绘制行星
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        shader.setMat4("model", model);
-        planet.Draw(shader);
+        planetShader.setMat4("model", model);
+        planet.Draw(planetShader);
 
         // 绘制小行星
-        for (unsigned int i = 0; i < amount; i++) {
-            shader.setMat4("model", modelMatrices[i]);
-            rock.Draw(shader);
+        asteroidShader.use();
+        asteroidShader.setInt("texture_diffuse1", 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id); // note: we also made the textures_loaded vector public (instead of private) from the model class.
+        for (unsigned int i = 0; i < rock.meshes.size(); i++) {
+            glBindVertexArray(rock.meshes[i].VAO);
+            glDrawElementsInstanced(GL_TRIANGLES, rock.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
+            glBindVertexArray(0);
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
