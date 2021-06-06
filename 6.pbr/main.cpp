f00671fe705f3001aeb888282ddf94ad67852c95
skychx <skychx@hotmail.com>
2021-06-06 19:59:04 +0800

    :sparkles:  6.2——「PBR」——「IBL」init

--- 6.pbr/main.cpp
+++ 6.pbr/main.cpp
@@ -1,7 +1,9 @@
 // #include "6.1.1.lighting.h"
-#include "6.1.2.lighting_textured.h"
+//#include "6.1.2.lighting_textured.h"
+#include "ibl_specular_textured.h"
 
 int main() {
     // run611();
-    run612();
+    // run612();
+    runibl();
 }
